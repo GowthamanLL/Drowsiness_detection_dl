8ed187c46de9ec43ef579887447287dbe342e7be
GowthamanLL <140971610+GowthamanLL@users.noreply.github.com>
2023-09-05 23:55:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,25 @@ if uploaded_file is not None:
     img_array = np.expand_dims(img_array, axis=0)
     img_array = img_array/255.0
     predictions = model.predict(img_array)
+    predictions = model.predict(img_array)
     predicted_label_index=np.argmax(predictions)
-    label=labels[predicted_label_index]
+    if (predicted_label_index==0):
+        label=('Drowsiness Detected')
+    elif (predicted_label_index==3):
+        label=('Drowsiness Detected')
+    elif (predicted_label_index==2) and (predicted_label_index==3):
+        label=('Drowsiness Detected')
+    elif (predicted_label_index==0) and (predicted_label_index==1):
+        label=('Drowsiness Detected')
+    elif (predicted_label_index==1) & (predicted_label_index==2):
+        label=('No Drowsiness Detected')
+    elif (predicted_label_index==0) and (predicted_label_index==3):
+        label=('Drowsiness Detected')
+    elif predicted_label_index==1:
+        label=('No Drowsiness Detected')
+    else:
+        label=("No Drowsiness Detected")
+
 
 
 st.write("### Prediction Result")
@@ -32,7 +49,7 @@ if st.button("Predict"):
         image1 = Image.open(uploaded_file)
         st.image(image1, caption="Uploaded Image", use_column_width=True)
         st.markdown(
-            f"<h2 style='text-align: center;'>Image of {label}</h2>",
+            f"<h2 style='text-align: center;'>{label} in given image.</h2>",
             unsafe_allow_html=True,
         )
     else:
@@ -53,6 +70,6 @@ if sample_img_choice:
     image1 = Image.open("d2.jpeg")
     st.image(image1, caption="Uploaded Image", use_column_width=True)    
     st.markdown(
-        f"<h2 style='text-align: center;'>{label}</h2>",
+        f"<h2 style='text-align: center;'>{label} in given image.</h2>",
         unsafe_allow_html=True,
     )
